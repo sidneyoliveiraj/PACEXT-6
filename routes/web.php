110e370598e13ab794ecb45fd5822343810344e0
sidneyoliveiraj <sidneycardosojo@gmail.com>
2025-09-12 12:10:26 -0300

    feat: Instala e configura o Laravel Livewire com componente de teste

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter; 
 
+
+// Esta é a rota que já existia (página inicial)
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
